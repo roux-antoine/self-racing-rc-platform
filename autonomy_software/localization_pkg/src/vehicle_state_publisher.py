b0ea104201568fa14720b42340cf09c1a817ac78
Antoine Roux <antoiroux@gmail.com>
2023-11-19 06:36:57 +0000

    implementation of the V2 of the future_pose computation

--- autonomy_software/localization_pkg/src/vehicle_state_publisher.py
+++ autonomy_software/localization_pkg/src/vehicle_state_publisher.py
@@ -8,13 +8,15 @@ In this case, the best is probably to not publish
 """
 
 import math
+import numpy as np
 import rospy
 import tf
+import time
 import utm
 
 from geometry_msgs.msg import PoseStamped, TwistStamped
 from nmea_msgs.msg import Gprmc
-from self_racing_car_msgs import ArduinoLogging
+from self_racing_car_msgs.msg import ArduinoLogging
 
 
 class VehicleStatePublisher:
@@ -45,11 +47,14 @@ class VehicleStatePublisher:
 
         self.last_msg_seq = None
         self.JUMPING_MESSAGE_FACTOR = 1
+        self._last_arduino_logging_msg = None
+        self._last_t_arduino_logging_msg = time.time()
 
         rospy.logwarn("Finished init")
 
     def callback_arduino_logging(self, arduino_msg: ArduinoLogging):
         self._last_arduino_logging_msg = arduino_msg
+        self._last_t_arduino_logging_msg = time.time()
 
     def callback_rmc_msg(self, rmc_msg: Gprmc):
 
@@ -78,12 +83,12 @@ class VehicleStatePublisher:
         x_utm, y_utm = utm_values[0], utm_values[1]
 
         """ Build the /current_pose [PoseStamped] message """
-        pose_msg = PoseStamped()
-        pose_msg.header.stamp = rospy.Time.now()
-        pose_msg.header.frame_id = "world"
-        pose_msg.pose.position.x = x_utm
-        pose_msg.pose.position.y = y_utm
-        pose_msg.pose.position.z = 0
+        current_pose_msg = PoseStamped()
+        current_pose_msg.header.stamp = rospy.Time.now()
+        current_pose_msg.header.frame_id = "world"
+        current_pose_msg.pose.position.x = x_utm
+        current_pose_msg.pose.position.y = y_utm
+        current_pose_msg.pose.position.z = 0
 
         # Yaw angle [radians]. Origin is horizontal axis. Counter clockwise
         # Offsetting by pi/2 compared to what is provided by the GPS, because the GPS track angle origin is the vertical axis
@@ -91,10 +96,10 @@ class VehicleStatePublisher:
 
         # Convert to quaternions
         quaternion = tf.transformations.quaternion_from_euler(0, 0, yaw_rad)
-        pose_msg.pose.orientation.x = quaternion[0]
-        pose_msg.pose.orientation.y = quaternion[1]
-        pose_msg.pose.orientation.z = quaternion[2]
-        pose_msg.pose.orientation.w = quaternion[3]
+        current_pose_msg.pose.orientation.x = quaternion[0]
+        current_pose_msg.pose.orientation.y = quaternion[1]
+        current_pose_msg.pose.orientation.z = quaternion[2]
+        current_pose_msg.pose.orientation.w = quaternion[3]
 
         """ Build the /current_velocity [TwistStamped] message """
         speed_mps = speed_knots * 0.5144  # m/s
@@ -113,18 +118,54 @@ class VehicleStatePublisher:
         )
 
         """ Publish topics """
-        self.pub_pose.publish(pose_msg)
+        self.pub_pose.publish(current_pose_msg)
         self.pub_velocity.publish(vel_msg)
 
         # computed the predicted pose
         dt = 0.1  # s
         future_pose_msg = PoseStamped()
-        future_pose_msg.header.stamp = rospy.Time.now()  # TODO figure out
+        future_pose_msg.header.stamp = (
+            rospy.Time.now()
+        )  # TODO figure out if this causes a problem
         future_pose_msg.header.frame_id = "world"
-        # TODO account for the command
-        future_pose_msg.pose.position.x = x_utm + speed_mps * math.cos(yaw_rad) * dt
-        future_pose_msg.pose.position.y = y_utm + speed_mps * math.sin(yaw_rad) * dt
+
+        # model 1
+        # future_pose_msg.pose.position.x = x_utm + speed_mps * math.cos(yaw_rad) * dt
+        # future_pose_msg.pose.position.y = y_utm + speed_mps * math.sin(yaw_rad) * dt
+        # future_pose_msg.pose.position.z = 0
+        # future_pose_msg.pose.orientation.x = quaternion[0]
+        # future_pose_msg.pose.orientation.y = quaternion[1]
+        # future_pose_msg.pose.orientation.z = quaternion[2]
+        # future_pose_msg.pose.orientation.w = quaternion[3]
+
+        # model 2, see the file future_state_computer.py for explanations
+        if self._last_arduino_logging_msg is not None and (
+            time.time() - self._last_t_arduino_logging_msg < 0.2
+        ):
+            steering_diff = self._last_arduino_logging_msg.steering_cmd_final - 98
+            if steering_diff != 0:
+                R = 15 / steering_diff
+            else:
+                R = 10000  # big number
+        else:
+            R = 10000  # big number
+
+        future_pose_msg.pose.position.x = (
+            x_utm
+            + np.cos(yaw_rad + np.pi / 2) * R * (np.cos(speed_mps / -R * dt) - 1)
+            - np.sin(yaw_rad + np.pi / 2) * R * np.sin(speed_mps / -R * dt)
+        )
+        future_pose_msg.pose.position.y = (
+            y_utm
+            + np.sin(yaw_rad + np.pi / 2) * R * (np.cos(speed_mps / -R * dt) - 1)
+            + np.cos(yaw_rad + np.pi / 2) * R * np.sin(speed_mps / -R * dt)
+        )
         future_pose_msg.pose.position.z = 0
+        future_pose_msg.pose.orientation.x = quaternion[0]
+        future_pose_msg.pose.orientation.y = quaternion[1]
+        future_pose_msg.pose.orientation.z = quaternion[2]
+        future_pose_msg.pose.orientation.w = quaternion[3]
+
         self.future_pub_pose.publish(future_pose_msg)
 
 
---
+++ start_scripts/start_rosbag_replay.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
+ABS_SCRIPT_DIR=$(readlink -f "$SCRIPT_DIR")
+NAME_WINDOW_1=""
+
+setup_bash_path="$ABS_SCRIPT_DIR/../../devel/setup.bash"
+
+session="self_racing_rc_platform-all"
+
+tmux new-session -d -s $session
+
+window=0
+tmux rename-window -t $session:$window 'Window1'
+
+# splitting the pane 0 vertically
+tmux split-window -t 0 -v
+# splitting the pane 0 vertically
+tmux split-window -t 0 -v
+tmux select-layout even-vertical
+# splitting the pane 0 horizontally
+tmux split-window -t 0 -h
+# splitting the pane 2 horizontally
+tmux split-window -t 2 -h
+# splitting the pane 4 horizontally
+tmux split-window -t 4 -h
+
+# roscore
+tmux select-pane -t 0
+tmux send-keys 'roscore' C-m
+# sleeping 10 seconds so that roscore has time to start
+echo "Waiting 10 seconds for roscore to start..."
+sleep 10
+
+# foxglove bridge
+tmux select-pane -t 1
+tmux send-keys "cd '$ABS_SCRIPT_DIR/../autonomy_software'" C-m
+tmux send-keys "source $setup_bash_path" C-m
+tmux send-keys "roslaunch foxglove_bridge foxglove_bridge.launch" C-m
+
+# map publisher
+tmux select-pane -t 2
+tmux send-keys "cd '$ABS_SCRIPT_DIR/../autonomy_software/localization_pkg/src'" C-m
+tmux send-keys "source $setup_bash_path" C-m
+tmux send-keys "rosrun localization_pkg map_publisher.py _map_file_name:=MapSanMateoP1.kml" C-m
+
+# vehicle state publisher
+tmux select-pane -t 3
+tmux send-keys "cd '$ABS_SCRIPT_DIR/../autonomy_software/localization_pkg/src'" C-m
+tmux send-keys "source $setup_bash_path" C-m
+tmux send-keys "rosrun localization_pkg vehicle_state_publisher.py" C-m
+
+# target generator
+tmux select-pane -t 4
+tmux send-keys "cd '$ABS_SCRIPT_DIR/../autonomy_software/planning_pkg/src'" C-m
+tmux send-keys "source $setup_bash_path" C-m
+tmux send-keys "rosrun planning_pkg target_generator.py _lookahead_distance:=5 _loop_over_waypoints:=True" C-m
+
+# lateral controller
+tmux select-pane -t 5
+tmux send-keys "cd '$ABS_SCRIPT_DIR/../autonomy_software/controllers_pkg/src'" C-m
+tmux send-keys "source $setup_bash_path" C-m
+tmux send-keys "rosrun controllers_pkg lateral_controller.py" C-m
+
+window=1
+tmux new-window -t $session:$window -n 'Window2'
+
+# splitting the pane 0 vertically
+tmux split-window -t 0 -v
+# splitting the pane 0 vertically
+tmux split-window -t 0 -v
+tmux select-layout even-vertical
+# splitting the pane 0 horizontally
+tmux split-window -t 0 -h
+# splitting the pane 2 horizontally
+tmux split-window -t 2 -h
+# splitting the pane 4 horizontally
+tmux split-window -t 4 -h
+
+
+
+# Longitudinal controller
+tmux select-pane -t 0
+tmux send-keys "cd '$ABS_SCRIPT_DIR/../autonomy_software/controllers_pkg/src'" C-m
+tmux send-keys "source $setup_bash_path" C-m
+tmux send-keys "rosrun controllers_pkg longitudinal_controller.py _longitudinal_control_mode:=1 _constant_pwm_output:=104 _speed_control_gain_p:=0.5 _speed_control_gain_i:=0.0" C-m
+
+# rosserial arduino
+tmux select-pane -t 1
+tmux send-keys "cd '$ABS_SCRIPT_DIR/../firmware'" C-m
+tmux send-keys "source $setup_bash_path" C-m
+tmux send-keys "rosrun rosserial_python serial_node.py _port:=/dev/ttyACM0 _baud:=57600" C-m
+
+# rostopic echo /arduino_logging
+tmux select-pane -t 2
+tmux send-keys "cd '$ABS_SCRIPT_DIR/../firmware'" C-m
+tmux send-keys "source $setup_bash_path" C-m
+tmux send-keys "rostopic echo /arduino_logging" C-m
+
+# for the rosbag play
+tmux select-pane -t 3
+
+
+# attaching the session
+tmux attach-session -t $session
