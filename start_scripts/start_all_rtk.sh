717b8f159e7654bad4a255b8e170a7ab4482df82
Antoine Roux <antoiroux@gmail.com>
2024-02-22 04:18:51 +0000

    created a new start script to start the infra

--- start_scripts/start_all_rtk.sh
+++ start_scripts/start_all_rtk.sh
@@ -26,48 +26,35 @@ tmux split-window -t 0 -h
 tmux split-window -t 2 -h
 # splitting the pane 4 horizontally
 tmux split-window -t 4 -h
-# # splitting the pane 6 horizontally
-tmux split-window -t 6 -h
-
-# roscore
-tmux select-pane -t 0
-tmux send-keys 'roscore' C-m
-# sleeping 10 seconds so that roscore has time to start
-echo "Waiting 10 seconds for roscore to start..."
-sleep 10
-
-# foxglove bridge
-tmux select-pane -t 1
-tmux send-keys "cd '$ABS_SCRIPT_DIR/../autonomy_software'" C-m
-tmux send-keys "source $setup_bash_path" C-m
-tmux send-keys "roslaunch foxglove_bridge foxglove_bridge.launch" C-m
-
-# rqt_reconfigure
-tmux select-pane -t 2
-tmux send-keys "sleep 10 && rosrun rqt_reconfigure rqt_reconfigure" C-m
 
 # map publisher
-tmux select-pane -t 3
+tmux select-pane -t 0
 tmux send-keys "cd '$ABS_SCRIPT_DIR/../autonomy_software/localization_pkg/src'" C-m
 tmux send-keys "source $setup_bash_path" C-m
 tmux send-keys "rosrun localization_pkg map_publisher.py _map_file_name:=MapSanMateoP1.kml" C-m
 
 # waypoint publisher
-tmux select-pane -t 4
+tmux select-pane -t 1
 tmux send-keys "cd '$ABS_SCRIPT_DIR/../autonomy_software/localization_pkg/src'" C-m
 tmux send-keys "source $setup_bash_path" C-m
 tmux send-keys "rosrun localization_pkg waypoints_publisher.py _waypoints_file:=wp_P1_loop_nov23_detail_3m.txt" C-m
 
 # rtcm correction data
-tmux select-pane -t 5
+tmux select-pane -t 2
 tmux send-keys "cd '$ABS_SCRIPT_DIR/../autonomy_software/localization_pkg/src'" C-m
 tmux send-keys "bash rtcm_corr_serial_pub.sh" C-m
 
 # gps listener
-tmux select-pane -t 6
+tmux select-pane -t 3
 tmux send-keys "cd '$ABS_SCRIPT_DIR/../autonomy_software'" C-m
 tmux send-keys "source $setup_bash_path" C-m
 tmux send-keys "rosrun localization_pkg rtk_gps_serial_reader.py" C-m
+
+# vehicle state publisher
+tmux select-pane -t 4
+tmux send-keys "cd '$ABS_SCRIPT_DIR/../autonomy_software/localization_pkg/src'" C-m
+tmux send-keys "source $setup_bash_path" C-m
+tmux send-keys "rosrun localization_pkg vehicle_state_publisher.py" C-m
 
 window=1
 tmux new-window -t $session:$window -n 'Window2'
@@ -84,38 +71,34 @@ tmux split-window -t 2 -h
 # splitting the pane 4 horizontally
 tmux split-window -t 4 -h
 
-# vehicle state publisher
-tmux select-pane -t 0
-tmux send-keys "cd '$ABS_SCRIPT_DIR/../autonomy_software/localization_pkg/src'" C-m
-tmux send-keys "source $setup_bash_path" C-m
-tmux send-keys "rosrun localization_pkg vehicle_state_publisher.py" C-m
+
 
 # target generator
-tmux select-pane -t 1
+tmux select-pane -t 0
 tmux send-keys "cd '$ABS_SCRIPT_DIR/../autonomy_software/planning_pkg/src'" C-m
 tmux send-keys "source $setup_bash_path" C-m
 tmux send-keys "rosrun planning_pkg target_generator.py" C-m
 
 # lateral controller
-tmux select-pane -t 2
+tmux select-pane -t 1
 tmux send-keys "cd '$ABS_SCRIPT_DIR/../autonomy_software/controllers_pkg/src'" C-m
 tmux send-keys "source $setup_bash_path" C-m
 tmux send-keys "rosrun controllers_pkg lateral_controller.py" C-m
 
 # Longitudinal controller
-tmux select-pane -t 3
+tmux select-pane -t 2
 tmux send-keys "cd '$ABS_SCRIPT_DIR/../autonomy_software/controllers_pkg/src'" C-m
 tmux send-keys "source $setup_bash_path" C-m
 tmux send-keys "rosrun controllers_pkg longitudinal_controller.py" C-m
 
 # rosserial arduino
-tmux select-pane -t 4
+tmux select-pane -t 3
 tmux send-keys "cd '$ABS_SCRIPT_DIR/../firmware'" C-m
 tmux send-keys "source $setup_bash_path" C-m
 tmux send-keys "rosrun rosserial_python serial_node.py _port:=/dev/ttyACM0 _baud:=57600" C-m
 
 # rostopic echo /arduino_logging
-tmux select-pane -t 5
+tmux select-pane -t 4
 tmux send-keys "cd '$ABS_SCRIPT_DIR/../firmware'" C-m
 tmux send-keys "source $setup_bash_path" C-m
 tmux send-keys "rostopic echo /arduino_logging" C-m
--- start_scripts/start_sim.sh
+++ start_scripts/start_sim.sh
@@ -6,7 +6,7 @@ NAME_WINDOW_1=""
 
 setup_bash_path="$ABS_SCRIPT_DIR/../../devel/setup.bash"
 
-session="self_racing_rc_platform-all"
+session="self_racing_rc_platform-sim"
 
 tmux new-session -d -s $session
 
@@ -25,37 +25,29 @@ tmux split-window -t 2 -h
 # splitting the pane 4 horizontally
 tmux split-window -t 4 -h
 
-# roscore
-tmux select-pane -t 0
-tmux send-keys 'roscore' C-m
-# sleeping 10 seconds so that roscore has time to start
-echo "Waiting 10 seconds for roscore to start..."
-sleep 10
-
-# foxglove bridge
-tmux select-pane -t 1
-tmux send-keys "cd '$ABS_SCRIPT_DIR/../autonomy_software/src'" C-m
-tmux send-keys "source $setup_bash_path" C-m
-tmux send-keys "roslaunch foxglove_bridge foxglove_bridge.launch" C-m
-
 # Simulator
-tmux select-pane -t 2
+tmux select-pane -t 0
 tmux send-keys "cd '$ABS_SCRIPT_DIR/../utils/vehicle_sim_pkg/src'" C-m
 tmux send-keys "source $setup_bash_path" C-m
 tmux send-keys "rosrun vehicle_sim_pkg sim.py" C-m
 
 # map publisher
-tmux select-pane -t 3
+tmux select-pane -t 1
 tmux send-keys "cd '$ABS_SCRIPT_DIR/../autonomy_software/localization_pkg/src'" C-m
 tmux send-keys "source $setup_bash_path" C-m
 tmux send-keys "rosrun localization_pkg map_publisher.py _map_file_name:=MapSanMateoP1.kml" C-m
 
 # waypoint publisher
-tmux select-pane -t 4
-tmux send-keys "cd '$ABS_SCRIPT_DIR/../autonomy_software/localization_pk/src'" C-m
+tmux select-pane -t 2
+tmux send-keys "cd '$ABS_SCRIPT_DIR/../autonomy_software/localization_pkg/src'" C-m
 tmux send-keys "source $setup_bash_path" C-m
 tmux send-keys "rosrun localization_pkg waypoints_publisher.py _waypoints_file:=waypoints_P1_straight_1_detail_3m_speed.txt" C-m
 
+# vehicle state publisher
+tmux select-pane -t 3
+tmux send-keys "cd '$ABS_SCRIPT_DIR/../autonomy_software/localization_pkg/src'" C-m
+tmux send-keys "source $setup_bash_path" C-m
+tmux send-keys "rosrun localization_pkg vehicle_state_publisher.py" C-m
 
 window=1
 tmux new-window -t $session:$window -n 'Window2'
@@ -72,26 +64,20 @@ tmux split-window -t 2 -h
 # splitting the pane 4 horizontally
 tmux split-window -t 4 -h
 
-# vehicle state publisher
-tmux select-pane -t 0
-tmux send-keys "cd '$ABS_SCRIPT_DIR/../autonomy_software/localization_pkg/src'" C-m
-tmux send-keys "source $setup_bash_path" C-m
-tmux send-keys "rosrun localization_pkg vehicle_state_publisher.py" C-m
-
 # target generator
-tmux select-pane -t 1
+tmux select-pane -t 0
 tmux send-keys "cd '$ABS_SCRIPT_DIR/../autonomy_software/planning_pkg/src'" C-m
 tmux send-keys "source $setup_bash_path" C-m
 tmux send-keys "rosrun planning_pkg target_generator.py" C-m
 
 # lateral controller
-tmux select-pane -t 2
+tmux select-pane -t 1
 tmux send-keys "cd '$ABS_SCRIPT_DIR/../autonomy_software/controllers_pkg/src'" C-m
 tmux send-keys "source $setup_bash_path" C-m
 tmux send-keys "rosrun controllers_pkg lateral_controller.py" C-m
 
 # Longitudinal controller
-tmux select-pane -t 3
+tmux select-pane -t 2
 tmux send-keys "cd '$ABS_SCRIPT_DIR/../autonomy_software/controllers_pkg/src'" C-m
 tmux send-keys "source $setup_bash_path" C-m
 tmux send-keys "rosrun controllers_pkg longitudinal_controller.py" C-m
