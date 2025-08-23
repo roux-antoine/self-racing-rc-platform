8ad1ef84b98f46a866734a3624038a6a0c392fd6
Antoine Roux <antoiroux@gmail.com>
2025-08-23 14:46:15 -0700

    moving to utils folder

--- utils/steering_param_identification/csv_processor.py
+++ utils/steering_param_identification/csv_processor.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 import plotly.figure_factory as ff
 import plotly.graph_objects as go
-import scipy
+import scipy  # noqa: F401
 from scipy.spatial import Delaunay
 from sklearn.linear_model import LinearRegression
 from sklearn.neighbors import KNeighborsRegressor
@@ -18,16 +18,16 @@ def model_v2(radius, current_velocity):
     bound_region_1 = 1.5
     bound_region_2 = 5
     bound_region_3 = 8
-    coeff_region_1 = 27 * 1.25 # max steering_diff * radius of circle at max lateral acceleration
+    coeff_region_1 = (
+        27 * 1.25
+    )  # max steering_diff * radius of circle at max lateral acceleration
     coeff_region_2 = 24 * 2.3
     coeff_region_3 = 26 * 4
     if current_velocity == 0:
         # not too sure what to do here, figure out
         coeff = 1000
     elif current_velocity > 0 and current_velocity <= bound_region_1:
-        coeff = 1 / (
-            coeff_region_1
-        )
+        coeff = 1 / (coeff_region_1)
     elif current_velocity > bound_region_1 and current_velocity <= bound_region_2:
         coeff = 1 / (
             coeff_region_1
@@ -38,13 +38,16 @@ def model_v2(radius, current_velocity):
     elif current_velocity > bound_region_2 and current_velocity <= bound_region_3:
         coeff = 1 / (
             coeff_region_2
-            + (current_velocity - bound_region_2) * (coeff_region_3 - coeff_region_2) / (bound_region_3 - bound_region_2)
+            + (current_velocity - bound_region_2)
+            * (coeff_region_3 - coeff_region_2)
+            / (bound_region_3 - bound_region_2)
         )
     elif current_velocity > bound_region_3:
         coeff = 1 / coeff_region_3
 
-    sterring_diff = 1 / radius / coeff 
+    sterring_diff = 1 / radius / coeff
     return min(27, sterring_diff)
+
 
 if __name__ == "__main__":
 
@@ -71,14 +74,13 @@ if __name__ == "__main__":
 
     max_accel = 9.81
     velocities = np.linspace(start=0, stop=10, num=20)
-    radiuses_at_limit = velocities ** 2 / max_accel
+    radiuses_at_limit = velocities**2 / max_accel
     fig = plt.figure()
     plt.scatter(speed, radius)
     plt.plot(velocities, radiuses_at_limit)
     plt.xlabel("speed")
     plt.ylabel("radius")
     plt.show()
-
 
     # Plotting in 3D with matplotlib
 
@@ -106,7 +108,6 @@ if __name__ == "__main__":
     # ax.set_zlabel("diff_to_neutral")
     # plt.show()
 
-
     # ###### delaunay stuff (useless) ######
     # points = []
     # for d, s, r in zip(diff_to_neutral, speed, radius):
@@ -128,10 +129,13 @@ if __name__ == "__main__":
         x=radius, y=speed, z=diff_to_neutral, mode="markers", marker=dict(size=5)
     )
     fig.add_trace(scatter_mesh)
-    fig.update_layout(scene=dict(xaxis_title='radius', yaxis_title='speed', zaxis_title='steering diff'))
+    fig.update_layout(
+        scene=dict(
+            xaxis_title="radius", yaxis_title="speed", zaxis_title="steering diff"
+        )
+    )
     fig.layout.scene.camera.projection.type = "orthographic"
     fig.show()
-
 
     # ###### Trying things out with prediction: linear regression ######
     X = []
@@ -164,57 +168,62 @@ if __name__ == "__main__":
 
     zs = []
     scatter_our_model = go.Scatter3d(
-            x=X_test[:,1],
-            y=X_test[:,2],
-            z=model_v1(X_test[:,1], X_test[:, 2]),
-            mode="markers",
-            marker=dict(size=5, color="blue"),
-        )
+        x=X_test[:, 1],
+        y=X_test[:, 2],
+        z=model_v1(X_test[:, 1], X_test[:, 2]),
+        mode="markers",
+        marker=dict(size=5, color="blue"),
+    )
 
     scatter_pred = go.Scatter3d(
-            x=X_test[:,1],
-            y=X_test[:,2],
-            z=predicted,
-            mode="markers",
-            marker=dict(size=5, color="red"),
-        )
+        x=X_test[:, 1],
+        y=X_test[:, 2],
+        z=predicted,
+        mode="markers",
+        marker=dict(size=5, color="red"),
+    )
 
     zs = []
-    for r, s in zip(X_test[:,1], X_test[:,2]):
+    for r, s in zip(X_test[:, 1], X_test[:, 2]):
         zs.append(model_v2(r, s))
     scatter_my_model = go.Scatter3d(
-            x=X_test[:,1],
-            y=X_test[:,2],
-            z=zs,
-            mode="markers",
-            marker=dict(size=5, color="orange"),
-        )
-
+        x=X_test[:, 1],
+        y=X_test[:, 2],
+        z=zs,
+        mode="markers",
+        marker=dict(size=5, color="orange"),
+    )
 
     fig.add_trace(scatter_mesh)
     # fig.add_trace(scatter_pred)
     # fig.add_trace(scatter_our_model)
     fig.add_trace(scatter_my_model)
-    fig.update_layout(scene=dict(xaxis_title='radius', yaxis_title='speed', zaxis_title='steering diff'))
+    fig.update_layout(
+        scene=dict(
+            xaxis_title="radius", yaxis_title="speed", zaxis_title="steering diff"
+        )
+    )
     fig.layout.scene.camera.projection.type = "orthographic"
     fig.show()
 
     # evaluate on measured set
-    predicted_our_model = model_v1(X[:,1], X[:, 2])
+    predicted_our_model = model_v1(X[:, 1], X[:, 2])
     predicted_my_model = []
-    for r, s in zip(X[:,1], X[:,2]):
+    for r, s in zip(X[:, 1], X[:, 2]):
         predicted_my_model.append(model_v2(r, s))
-    
+
     print(predicted_our_model)
     fig = go.Figure()
 
-    fig.add_trace(go.Scatter3d(
-            x=X[:,1],
-            y=X[:,2],
+    fig.add_trace(
+        go.Scatter3d(
+            x=X[:, 1],
+            y=X[:, 2],
             z=predicted_our_model - y,
             mode="markers",
             marker=dict(size=5, color="blue"),
-        ))
+        )
+    )
     # fig.add_trace(go.Scatter3d(
     #         x=X[:,1],
     #         y=X[:,2],
@@ -226,9 +235,10 @@ if __name__ == "__main__":
 
     print(np.linalg.norm(predicted_our_model - y))
     print(np.linalg.norm(predicted_my_model - y))
-    fig.update_layout(scene=dict(xaxis_title='radius', yaxis_title='speed', zaxis_title='error'))
+    fig.update_layout(
+        scene=dict(xaxis_title="radius", yaxis_title="speed", zaxis_title="error")
+    )
     fig.show()
-
 
     # ###### Trying things out with prediction: k nearest neighbors ######
 
@@ -256,13 +266,17 @@ if __name__ == "__main__":
     predicted = knn_model.predict(X_test)
     print(predicted)
     scatter_pred = go.Scatter3d(
-            x=X_test[:,0],
-            y=X_test[:,1],
-            z=predicted,
-            mode="markers",
-            marker=dict(size=5, color="red"),
-        )
+        x=X_test[:, 0],
+        y=X_test[:, 1],
+        z=predicted,
+        mode="markers",
+        marker=dict(size=5, color="red"),
+    )
     fig.add_trace(scatter_mesh)
     fig.add_trace(scatter_pred)
-    fig.update_layout(scene=dict(xaxis_title='radius', yaxis_title='speed', zaxis_title='steering diff'))
+    fig.update_layout(
+        scene=dict(
+            xaxis_title="radius", yaxis_title="speed", zaxis_title="steering diff"
+        )
+    )
     fig.show()
