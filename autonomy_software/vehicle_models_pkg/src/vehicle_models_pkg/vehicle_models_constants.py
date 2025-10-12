56aa4e24426b0829897da0fe6b219ba4f59c7f92
Antoine Roux <antoiroux@gmail.com>
2025-10-11 22:17:37 -0700

    cleanup

--- autonomy_software/vehicle_models_pkg/src/vehicle_models_pkg/vehicle_models_constants.py
+++ autonomy_software/vehicle_models_pkg/src/vehicle_models_pkg/vehicle_models_constants.py
@@ -8,7 +8,7 @@ EFFECTIVE_MAX_STEERING_ANGLE_RAD_V1 = (
 STEERING_PWM_IDLE = 90
 STEERING_PWM_MIN = 64  # 62 + 2
 STEERING_PWM_MIN = 116  # 118 - 2
-STEERING_DIRECTION_FACTOR = -1  # TODO explain
+STEERING_DIRECTION_FACTOR = -1  # Used because a positive change in steering command leads to a negative change in steering angle
 
 WHEELBASE = 0.406
 PWM_DIFF_AT_MAX_STEER_ANGLE = 27
